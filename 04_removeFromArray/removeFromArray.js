1425bf94700e5b1cc7c33ea954516f0c95c546e9
julianty <alexanderjulianty@gmail.com>
2022-04-27 10:44:03 -0700

    Completes exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArray, ...targets) {
+  let outputArray = inputArray;
+  targets.forEach(item => {
+    if (!(outputArray.indexOf(item) == -1)) {
+      outputArray.splice(outputArray.indexOf(item), 1)
+    }
+  });
+  return outputArray
 };
 
 // Do not edit below this line
