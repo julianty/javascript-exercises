b0f8e3e8f61a350dccf11f880f94dc608f8688a6
julianty <alexanderjulianty@gmail.com>
2022-04-27 11:05:37 -0700

    Adjusts formatting in file

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,13 @@
 const removeFromArray = function(inputArray, ...targets) {
   let outputArray = inputArray;
+
   targets.forEach(item => {
+    // Checks if item is present in array
     if (!(outputArray.indexOf(item) == -1)) {
       outputArray.splice(outputArray.indexOf(item), 1)
     }
   });
+
   return outputArray
 };
 
