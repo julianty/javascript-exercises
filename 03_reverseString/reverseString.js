c52c895d3b88f9771b6dc9f145a81920c1a10080
julianty <alexanderjulianty@gmail.com>
2022-04-27 10:26:09 -0700

    Completes exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
+    let inputStringArray = inputString.split("")
+    let outputStringArray = [];
 
+    inputStringArray.forEach(character => 
+        outputStringArray.splice(0, 0, character)
+    );
+
+    return outputStringArray.join("");
 };
 
 // Do not edit below this line
