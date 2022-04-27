2f825c0fb61fd1456ced8b310e0d5b3749377ed2
julianty <alexanderjulianty@gmail.com>
2022-04-27 10:51:33 -0700

    Completes exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+  if (typeof(start) != typeof(1) || typeof(end) != typeof(1)) return "ERROR";
+  if (start < 0 || end < 0) return "ERROR";
 
+  let sum = 0;
+  if (start < end) {
+    for (let i=start; i<=end; i++) {
+      sum += i;
+    }
+  } else {
+    for (let i=end; i<=start; i++) {
+      sum += i;
+    }
+
+  }
+  
+  return sum
 };
 
 // Do not edit below this line
