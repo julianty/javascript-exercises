843a3b3c3a012f0c42dbb0ababe796048d8f7b9a
julianty <alexanderjulianty@gmail.com>
2022-04-27 10:10:03 -0700

    Completes exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(inputString, repetitions) {
+    let outputString = "";
+    for (let i=0; i<repetitions; i++) {
+        outputString += inputString;
+    }
+    if (repetitions < 0) {
+        outputString = "ERROR"
+    }
+    return outputString
 };
 
 // Do not edit below this line
