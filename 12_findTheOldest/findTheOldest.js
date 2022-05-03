73940da0810d580158d1524e406354c5b33c3f17
julianty <alexanderjulianty@gmail.com>
2022-05-03 11:56:17 -0700

    Completes exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  const currentYear = 2022;
+  people = people.sort((personA, personB) => {
+    let ageA = ageB = 0;
+    if (!("yearOfDeath" in personA)) {
+      ageA = currentYear - personA.yearOfBirth;
+    } else {
+      ageA = personA.yearOfDeath - personA.yearOfBirth;
+    }
+    if (!("yearOfDeath" in personB)) {
+      ageB = currentYear - personB.yearOfBirth;
+    } else {
+      ageB = personB.yearOfDeath - personB.yearOfBirth;
+    }
 
+    return ageA > ageB ? -1 : 1;
+  });
+  // return people.slice(0, 1);
+  return people[0];
+  // console.log(people[0].name);
 };
 
 // Do not edit below this line
