42799714af1d62efe927a3fb508d3e05f89527e4
julianty <alexanderjulianty@gmail.com>
2022-05-03 10:44:40 -0700

    Completes this exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(sumArray) {
+	return sumArray.reduce((sum, arrayItem) => sum += arrayItem, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(multArray) {
+ return multArray.reduce((product, arrayItem) => product *= arrayItem, 1) ;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let factorialProduct = 1;
+  for(let i=1; i<=a; i++) {
+    factorialProduct *= i;
+  };
+  return factorialProduct
 };
 
 // Do not edit below this line
