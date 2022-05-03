8f19d97ce7dc856537efbbae0ba04925571f105c
julianty <alexanderjulianty@gmail.com>
2022-05-03 11:27:19 -0700

    Completes exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(nthmember) {
+  // return nthmember == 1 ? answer : fibonacci(nthmember)A
+  if (nthmember < 1) return "OOPS"
+  if (typeof(nthmember) == typeof("string")){
+    nthmember = parseInt(nthmember);
+  }
+  return nthmember <= 2 ? 1 : fibonacci(nthmember - 1) + fibonacci(nthmember - 2)
 };
 
 // Do not edit below this line
