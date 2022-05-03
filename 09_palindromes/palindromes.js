d82dd2cbf8a2b7d8a5b06cc6c62c71c64fd861f6
julianty <alexanderjulianty@gmail.com>
2022-05-03 11:07:03 -0700

    Completes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (inputString) {
+  // remove punctuation
+  inputString = inputString.replaceAll(/[,!.\s]/ig, "")
+  inputString = inputString.toLowerCase();
+  console.log(inputString);
 
+  // const reversedString = inputString.reduce((finalString, char))
+  let reversedString = [];
+  Array.from(inputString).forEach( char => 
+    reversedString.splice(0, 0, char)
+  );
+  reversedString = reversedString.join('');
+
+  
+
+  return reversedString == inputString ? true : false
 };
 
 // Do not edit below this line
